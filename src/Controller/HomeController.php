7cbf9b0c174d54a76bbea311c54ae2df555c48b9
Michel Theo <theo.michel2@outlook.fr>
2023-08-25 19:24:55 +0200

    cancel user login if unverifed

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,12 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        if($this->getUser() && !$this->getUser()->isVerified()){
+            $this->addFlash('error',"Veuillez vérifiez votre adresse mail");
+            return $this->redirectToRoute('app_logout');
+        }
+
+        var_dump($this->getUser());
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
