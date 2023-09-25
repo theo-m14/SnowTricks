7a4e58b849249ad095d247555dc8a95e3fbb4e80
Michel Theo <theo.michel2@outlook.fr>
2023-09-25 12:13:59 +0200

    tricks FindAll on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TricksRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,14 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TricksRepository $tricksRepository): Response
     {
         if($this->getUser() && !$this->getUser()->isVerified()){
             $this->addFlash('unverified',"Veuillez vérifiez votre adresse mail");
         }
 
+        $tricks = $tricksRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'HomeController','tricks'=> $tricks
         ]);
     }
 }
